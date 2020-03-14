02330f4b718513b5b99eaab7349bdd5d4b44a49a
Amelia Davis <ameliaaltman22@gmail.com>
2020-03-13 23:28:28 -0600

    added code for tables in schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE  employees_db;
+
+USE employees_db;
+
+CREATE TABLE  department
+(
+    id int NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role 
+(
+    id int NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(6,2), 
+    department_id int,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee 
+(
+    id int NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL, 
+    role_id int,
+    manager_id int,
+    PRIMARY KEY (id)
+)
